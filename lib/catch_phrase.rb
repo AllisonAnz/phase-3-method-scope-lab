ea5a662b8e6e00a4cc6852f46d1b0f946194339e
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-04 11:48:24 -0500

    commit tree fabae63f5305432539aed310dd88773d9d12ae7b

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,12 @@
-def catch_phrase
+
+
+#catch_phrase
+  # puts out a catch phrase 
+phrase = "It's-a me, Mario!"
+
+def catch_phrase(phrase)
   puts phrase
 end
+
+catch_phrase(phrase)
+#=> It's-a me, Mario!
--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -7,3 +7,9 @@ end
 
 # rescue_princess_peach
 # puts status
+
+# Hooray! Mario has rescued Princess Peach.
+# lib/rescue_princess_peach.rb:9:in `<main>': undefined local variable or method `status' for main:Object (NameError) 
+
+# We get a name error b/c status is undefined 
+# The variable is defined inside a method and are not available outside the method
